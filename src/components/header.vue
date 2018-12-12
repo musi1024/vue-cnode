0d03b6fc948a26e6ebfebf0dcebfd144474d3539
musi <957028613@qq.com>
2018-12-12 14:55:31 +0800

    init header

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,28 +1,42 @@
 <template>
-    <div id="header"> 
-        <img src="https://static2.cnodejs.org/public/images/cnodejs_light.svg" alt="">
-        <ul>
-            <li>首页</li>
-            <li>新手入门</li>
-            <li>API</li>
-            <li>关于</li>
-            <li>注册</li>
-            <li>登录</li>
-        </ul>
+  <div id="header">
+    <div class="container">
+      <img src="https://static2.cnodejs.org/public/images/cnodejs_light.svg" alt>
+      <ul>
+        <li>首页</li>
+        <li>新手入门</li>
+        <li>API</li>
+        <li>关于</li>
+        <li>注册</li>
+        <li>登录</li>
+      </ul>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'Header'
-    }
+export default {
+  name: "Header"
+};
 </script>
 
 <style lang='scss' scoped>
-    #header {
-        color:red;
-        li {
-            border: 1px solid green;
-        }
-    }
+#header {
+  background-color: rgb(68, 68, 68);
+  .container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px 64px;
+  }
+  img {
+    width: 110px;
+  }
+  li {
+    color: #ccc;
+    display: inline-block;
+    padding: 0 10px;
+    font-size: 13px;
+  }
+}
 </style>
