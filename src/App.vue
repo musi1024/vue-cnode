50b0e57bdbdd064a5dd9a5ed8e0fcf08c27548c5
musi <957028613@qq.com>
2018-12-12 14:01:57 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Header></Header>
+  </div>
+</template>
+
+<script>
+import Header from './components/header'
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+@import './assets/common';
+</style>
---
+++ src/components/header.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="header"> 
+        <img src="https://static2.cnodejs.org/public/images/cnodejs_light.svg" alt="">
+        <ul>
+            <li>首页</li>
+            <li>新手入门</li>
+            <li>API</li>
+            <li>关于</li>
+            <li>注册</li>
+            <li>登录</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Header'
+    }
+</script>
+
+<style lang='scss' scoped>
+    #header {
+        color:red;
+        li {
+            border: 1px solid green;
+        }
+    }
+</style>
